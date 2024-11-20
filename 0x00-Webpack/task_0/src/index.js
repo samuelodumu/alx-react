9456a77f7a5fbe3bccad82c3d07fc1983ac657d1
Samuel Odumu <themainsamuel@gmail.com>
2024-11-20 14:01:32 +0100

    task 0: Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,12 +1,5 @@
 import $ from 'jquery';
-const p1 = 'Holberton Dashboard';
-const p2 = 'Dashboard data for the students';
-const p3 = 'Copyright - Holberton School';
 
-const first = $('<p></p>').text(p1);
-const second = $('<p></p>').text(p2);
-const third = $('<p></p>').text(p3);
-
-$('body').append(first);
-$('body').append(second);
-$('body').append(third);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
