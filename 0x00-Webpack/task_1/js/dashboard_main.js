6a0c6c15a342882bde018321bcf26905d76805d0
Samuel Odumu <themainsamuel@gmail.com>
2024-11-25 23:49:20 +0100

    task 1: finished jquery instructions

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
