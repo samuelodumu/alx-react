d3901b796a60c7abd03bfacd9fd6d0422eb001f2
Samuel Odumu <themainsamuel@gmail.com>
2024-11-26 00:17:51 +0100

    task 1: prevented spammers

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,3 +4,12 @@ $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+  const count = $('#count').html() || 0;
+    $('#count').html(parseInt(count) + 1 + ' clicks on the button');
+}
+
+const debounce = _.debounce(updateCounter, 1000);
+
+$('button').on('click', debounce);
