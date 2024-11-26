e4e5fed847f27f3b05b8f6d11e55f4390b6a4ed3
Samuel Odumu <themainsamuel@gmail.com>
2024-11-26 03:59:38 +0100

    task 1: Learning how to use Webpack with a config file

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Holberton Dashboard',
+            template: 'index.html'
+        })
+    ],
+};
