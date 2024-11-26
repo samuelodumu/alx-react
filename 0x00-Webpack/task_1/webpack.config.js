797d53a39208baf6e40194cbcd44eda876a02354
Samuel Odumu <themainsamuel@gmail.com>
2024-11-26 04:43:48 +0100

    task 1: removed a plugin

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -8,9 +8,4 @@ module.exports = {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Holberton Dashboard',
-        })
-    ],
 };
