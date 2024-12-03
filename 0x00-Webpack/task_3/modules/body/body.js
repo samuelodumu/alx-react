f336d9e66426a817fa66cb2b9e859da67235ad60
Samuel Odumu <themainsamuel@gmail.com>
2024-12-04 00:42:21 +0100

    done with body module

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,15 @@
+import $ from jQuery;
+import _ from lodash;
+import './body.css';
+
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+    const count = $('#count').html() || 0;
+    $('#count').html(parseInt(count) + 1 + ' clicks on the button');
+}
+const debounced_function = _.debouned(updateCounter, 500);
+$('button').on('click', debounced_function);
