885cc806ad99ed04e6e82c1d0e3946efb2d10163
Samuel Odumu <themainsamuel@gmail.com>
2024-12-03 19:35:09 +0100

    imported loadash in task_1/dashboard_main.js

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+  const count = $('#count').html() || 0;
+  $('#count').html(parseInt(count) + 1 + ' clicks on the button');
+}
+
+const debounced = _.debounce(updateCounter, 500);
+
+$('button').on('click', debounced);
