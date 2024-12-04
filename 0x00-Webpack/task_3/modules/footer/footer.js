8206044d45714d358db7103b38a5bb5ee3f81de2
Samuel Odumu <themainsamuel@gmail.com>
2024-12-04 01:25:40 +0100

    divided code into  modules

---
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,3 @@
+import $ from 'jquery';
+import './footer.css';
+$('body').append('<p>Copyright - Holberton School</p>');
--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,23 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { plugins } = require('../demo/webpack.config');
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
+  mode: 'development', // set mode option, 'development' or 'production'
+  entry: {
+    header: './modules/header/header.js', // entry point for header.js
+    body: './modules/body/body.js', // entry point for body.js
+    footer: './modules/footer/footer.js' // entry point for footer.js
+  },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'public'), // output path
+    filename: '[name].bundle.js' // output filename
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public') // static files
+    },
+    port: 8564, // port number
+    open: true, // open browser
+    hot: true, // hot module replacement
+    compress: true // enable gzip compression
   },
 
   module: {
     rules: [
+      // loaders
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/, // files with .css extension
+        use: ['style-loader', 'css-loader'] // use style-loader and css-loader
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        use: ['file-loader', 'image-webpack-loader']
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i, // files with .ico, .gif, .png, .jpg, .jpeg extension
+        type: 'asset/resource' // use asset/resource
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard' // title of the html page
+    })
+  ]
 };
