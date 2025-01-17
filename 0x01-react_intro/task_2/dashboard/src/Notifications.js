07c715ffa7e19ab8ed5f0fae1ece7876242959df
Samuel Odumu <themainsamuel@gmail.com>
2025-01-17 05:25:12 +0100

    task 4

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Notifications.css';
+import { getLatestNotification } from './utils';
+import closeIcon from './close-icon.png';
 
 function Notifications() {
   return (
     <div className="Notifications">
+      <button aria-label='close' style={{ float: 'right', cursor: 'pointer' }} onClick={console.log('Close button has been clicked')}>
+        <img src={closeIcon} alt='close-icon' />
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li data-priority='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>  
+      </ul>
     </div>
   );
 }
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,13 +1,17 @@
 function getFullYear() {
-    return new Date().getFullYear();
+  return new Date().getFullYear();
 }
 
 function getFooterCopy(isIndex) {
-    if (isIndex) {
-        return 'ALX';
-    } else {
-        return 'ALX main dashboard';
-    }
+  if (isIndex) {
+    return 'ALX';
+  } else {
+    return 'ALX main dashboard';
+  }
 }
 
-export { getFullYear, getFooterCopy };
+function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
+
+export { getFullYear, getFooterCopy, getLatestNotification };
